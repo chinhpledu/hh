11036599f3fe368ea7f4c4e7b3cf82a4d93d5b94
RJC <chinhpl@rjc.co.jp>
2019-06-20 17:38:55 +0900

    Finish setup microservices

---
+++ socket/start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#
+export GOPATH=/var/www/html/core
+export PATH=$PATH:$GOPATH/bin
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#
-export GOPATH=/var/www/html/core
-export PATH=$PATH:$GOPATH/bin
